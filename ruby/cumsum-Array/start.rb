3b3fe0cc227af9d4c46a2154b97af81a072fc4bf
Dan Nuffer <danielnuffer@gmail.com>
2012-07-31 07:48:51 -0600

    Add ruby/cumsum-Array

---
+++ ruby/cumsum-Array/start.rb
@@ -0,0 +1,17 @@
+require "test/unit/assertions"
+
+
+module Enumerable
+  # Returns a Array whose elements are the cumulative sums of the enum
+  def cumsum
+    
+  end
+end
+
+include Test::Unit::Assertions
+
+assert_equal([].cumsum, [])
+assert_equal([0].cumsum, [0])
+assert_equal([10].cumsum, [10])
+assert_equal([1,2,3].cumsum, [1,3,6])
+assert_equal([1,-1,2].cumsum, [1,0,2])
