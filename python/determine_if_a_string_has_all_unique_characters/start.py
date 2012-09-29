aae99e281c2f8ad089e583f442cbbadd9f140577
Dan Nuffer <danielnuffer@gmail.com>
2012-09-29 07:57:53 -0600

    Add python/determine_if_a_string_has_all_unique_characters

---
+++ python/determine_if_a_string_has_all_unique_characters/start.py
@@ -0,0 +1,17 @@
+"""
+
+>>> has_unique_characters("abc")
+True
+>>> has_unique_characters("aaa")
+False
+>>> has_unique_characters(u"abc")
+True
+>>> has_unique_characters(u"aaa")
+False
+"""
+
+def has_unique_characters(s):
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
