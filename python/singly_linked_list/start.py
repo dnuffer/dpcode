7ed611ddcd465323f8656fa0c5e1391f566bd691
Dan Nuffer <danielnuffer@gmail.com>
2012-11-12 08:01:07 -0700

    Add python/singly_linked_list

---
+++ python/singly_linked_list/start.py
@@ -0,0 +1,35 @@
+"""
+>>> l=singly_linked_list()
+>>> l.data = 10
+>>> l.data
+10
+>>> l.next == None
+True
+>>> l.append(20)
+>>> l.next.data
+20
+>>> l.next.next == None
+True
+
+"""
+
+# TODO: Implement a singly-linked list with next, data, and append()
+class singly_linked_list:
+  def __init__(self):
+    self.data = None
+    self.next = None
+
+  def append(self, data):
+    cur = self
+    while cur.next != None:
+      cur = cur.next
+    cur.next = singly_linked_list()
+    cur.next.data = data
+    
+			
+
+if __name__ == '__main__':
+	import doctest
+	if doctest.testmod().failed > 0:
+		import sys
+		sys.exit(1)
