98a7ad0b07839a719d60cc486779bd1645b2d37e
Dan Nuffer <danielnuffer@gmail.com>
2012-12-13 07:32:19 -0700

    Add a single class singly_linked_list which is the cumulation of all the exercises.

---
+++ python/singly_linked_list_complete/singly_linked_list.py
@@ -0,0 +1,167 @@
+class singly_linked_list_node:
+  def __init__(self, data = None, next_node = None):
+    self.data = data
+    self.next_node = next_node
+
+class singly_linked_list:
+  def __init__(self, items = None):
+    self.head = None
+    self.tail = None
+
+    if items:
+      for item in items:
+        self.append(item)
+
+  def append(self, data):
+    """
+    >>> l=singly_linked_list()
+    >>> print l.append(0)
+    [0]
+    >>> print l.append(None)
+    [0 -> None]
+    >>> print l.append("a")
+    [0 -> None -> a]
+    """
+    if self.tail:
+      self.tail.next_node = singly_linked_list_node()
+      self.tail = self.tail.next_node
+    else:
+      self.head = self.tail = singly_linked_list_node()
+
+    self.tail.data = data
+    return self
+    
+  def delete(self, data):
+    """
+    >>> l=singly_linked_list([10, 20])
+    >>> print l.delete(10)
+    [20]
+    >>> print l.delete(10)
+    [20]
+    >>> print l.delete(20)
+    []
+    >>> print l.delete(1)
+    []
+    >>> l=singly_linked_list([1, 2, 3])
+    >>> print l.delete(1)
+    [2 -> 3]
+    >>> l=singly_linked_list([1, 2, 3])
+    >>> print l.delete(2)
+    [1 -> 3]
+    >>> l=singly_linked_list([1, 2, 3])
+    >>> print l.delete(3)
+    [1 -> 2]
+    >>> print l.delete(2)
+    [1]
+    >>> print l.delete(1)
+    []
+    >>> print l.delete(None)
+    []
+    """
+    if not self.head:
+      return self
+
+    if self.head.data == data:
+      if self.head == self.tail:
+        self.head = self.tail = None
+      else:
+        self.head = self.head.next_node
+      return self
+			
+    node = self.head
+
+    while node.next_node:
+      if node.next_node.data == data:
+        if node.next_node.next_node == None:
+          self.tail = node
+        node.next_node = node.next_node.next_node
+        return self
+
+      node = node.next_node
+
+    return self
+
+  def __len__(self):
+    """
+    >>> len(singly_linked_list([]))
+    0
+    >>> len(singly_linked_list([1]))
+    1
+    >>> len(singly_linked_list(range(10)))
+    10
+    """
+    length = 0
+    node = self.head
+    while node:
+      length += 1
+      node = node.next_node
+    return length
+
+  def __getitem__(self, k):
+    """
+    >>> sll = singly_linked_list([1])
+    >>> print sll[0]
+    1
+    >>> sll = singly_linked_list([1, 2, 3])
+    >>> print sll[0]
+    1
+    >>> print sll[1]
+    2
+    >>> print sll[2]
+    3
+    >>> sll = singly_linked_list(["A", "b", "C"])
+    >>> sll[0]
+    'A'
+    >>> sll[-1]
+    'C'
+    >>> sll[-2]
+    'b'
+    >>> sll[-3]
+    'A'
+    >>> sll[-4]
+    Traceback (most recent call last):
+      ...
+    IndexError: list index out of range
+    >>> sll[10]
+    Traceback (most recent call last):
+      ...
+    IndexError: list index out of range
+    """
+    if k >= 0:
+      node = self.head
+      while node != None and k > 0:
+        node = node.next_node
+        k -= 1
+      if k > 0:
+        raise IndexError("list index out of range")
+      return node.data
+    else:
+      length = len(self)
+      if k * -1 > length:
+        raise IndexError("list index out of range")
+      return self[length + k]
+
+  def __iter__(self):
+    node = self.head
+    while node != None:
+      yield node
+      node = node.next_node
+    return
+
+  def __str__(self):
+    """
+    >>> print singly_linked_list([0, None, "a"])
+    [0 -> None -> a]
+    """
+    return '[%s]' % ' -> '.join([str(node.data) for node in self])
+
+if __name__ == '__main__':
+  import doctest
+  result = doctest.testmod()
+  if result.failed > 0:
+    print "Failed:", result
+    import sys
+    sys.exit(1)
+  else:
+    print "Success:", result
+
