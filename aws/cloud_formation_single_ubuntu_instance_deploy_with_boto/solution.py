5c5eb96fce50337f2338e400921d98fa6f46633d
Dan Nuffer <danielnuffer@gmail.com>
2013-01-18 09:34:43 -0700

    Can't use _ in stack names.

--- aws/cloud_formation_single_ubuntu_instance_deploy_with_boto/solution.py
+++ aws/cloud_formation_single_ubuntu_instance_deploy_with_boto/solution.py
@@ -5,7 +5,7 @@
 ...     instance.create_stack.return_value = 'the result'
 ...     result = deploy()
 ...     mock.assert_called_with('us-west-2')
-...     instance.create_stack.assert_called_once_with('dpcode_boto', template_body=open('ubuntu.template', 'r').read())
+...     instance.create_stack.assert_called_once_with('dpcode-boto', template_body=open('ubuntu.template', 'r').read())
 ...     print result
 the result
 """
@@ -13,7 +13,7 @@ the result
 def deploy():
   import boto.cloudformation
   conn = boto.cloudformation.connect_to_region('us-west-2')
-  stack_id = conn.create_stack('dpcode_boto', template_body=open('ubuntu.template', 'r').read())
+  stack_id = conn.create_stack('dpcode-boto', template_body=open('ubuntu.template', 'r').read())
   return stack_id
 
 
--- aws/cloud_formation_single_ubuntu_instance_deploy_with_boto/start.py
+++ aws/cloud_formation_single_ubuntu_instance_deploy_with_boto/start.py
@@ -5,12 +5,12 @@
 ...     instance.create_stack.return_value = 'the result'
 ...     result = deploy()
 ...     mock.assert_called_with('us-west-2')
-...     instance.create_stack.assert_called_once_with('dpcode_boto', template_body=open('ubuntu.template', 'r').read())
+...     instance.create_stack.assert_called_once_with('dpcode-boto', template_body=open('ubuntu.template', 'r').read())
 ...     print result
 the result
 """
 
-# TODO: create a CloudFormation stack in us-west-2 named dpcode_boto with the template in ubuntu.template
+# TODO: create a CloudFormation stack in us-west-2 named dpcode-boto with the template in ubuntu.template
 
 
 if __name__ == '__main__':
