b1df53207eb4b31c6907e96c2c65c2c51016f089
Dan Nuffer <danielnuffer@gmail.com>
2013-01-18 09:35:48 -0700

    Add a mind-map of how the exercises build on each other.

---
+++ aws/cloud_formation_relationships.mm
@@ -0,0 +1,18 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1358523969088" ID="ID_501866575" MODIFIED="1358524038734" TEXT="cloud_formation_single_ubuntu_instance">
+<node CREATED="1358524213782" ID="ID_1184154377" MODIFIED="1358524228620" POSITION="right" TEXT="_region_instance_mapping">
+<node CREATED="1358524279398" ID="ID_151508335" MODIFIED="1358524281579" TEXT="_metadata">
+<node CREATED="1358524407770" ID="ID_1332555805" MODIFIED="1358524421478" TEXT="_user_data">
+<node CREATED="1358524458704" ID="ID_694801716" MODIFIED="1358524461660" TEXT="_description">
+<node CREATED="1358524987403" ID="ID_1699159032" MODIFIED="1358524989145" TEXT="_outputs">
+<node CREATED="1358525017733" ID="ID_474475390" MODIFIED="1358525022209" TEXT="_ephemeral_storage"/>
+<node CREATED="1358525107960" ID="ID_501617616" MODIFIED="1358525189077" TEXT="_register_hostname_in_dns"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1358524904515" ID="ID_1611740872" MODIFIED="1358524912783" POSITION="left" TEXT="_deploy_with_boto"/>
+</node>
+</map>
