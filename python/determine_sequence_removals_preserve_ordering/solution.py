0fb7b6e7962521355f8d5cd7b6492529535d220d
Dan Nuffer <danielnuffer@gmail.com>
2013-01-27 08:09:45 -0700

    Add python/determine_sequence_removals_preserve_ordering

---
+++ python/determine_sequence_removals_preserve_ordering/solution.py
@@ -0,0 +1,22 @@
+"""
+>>> find_removals([1,2,3], [2,3,4])
+[1]
+>>> find_removals([1,2,3], [3,4,5])
+[1, 2]
+>>> find_removals([1,2,3], [])
+[1, 2, 3]
+>>> find_removals([3,2,1], [])
+[3, 2, 1]
+>>> find_removals([], [1,2,3])
+[]
+"""
+
+def find_removals(prev, cur):
+  cur_set = set(cur)
+  return filter(lambda item: item not in cur_set, prev)
+
+if __name__ == '__main__':
+	import doctest
+	if doctest.testmod().failed > 0:
+		import sys
+		sys.exit(1)
---
+++ python/determine_sequence_removals_preserve_ordering/start.py
@@ -0,0 +1,22 @@
+"""
+>>> find_removals([1,2,3], [2,3,4])
+[1]
+>>> find_removals([1,2,3], [3,4,5])
+[1, 2]
+>>> find_removals([1,2,3], [])
+[1, 2, 3]
+>>> find_removals([3,2,1], [])
+[3, 2, 1]
+>>> find_removals([], [1,2,3])
+[]
+"""
+
+# TODO: Write find_removals() which returns a list containing the items that are in the first sequence 
+# parameter and not in the second sequence parameter which preserves the order of items in the first
+# sequence
+
+if __name__ == '__main__':
+	import doctest
+	if doctest.testmod().failed > 0:
+		import sys
+		sys.exit(1)
