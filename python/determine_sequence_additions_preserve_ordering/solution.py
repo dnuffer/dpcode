e496ae0ec5cbf6a7e501e209c86e1b2c8e8b0103
Dan Nuffer <danielnuffer@gmail.com>
2013-01-27 07:59:29 -0700

    Add python/determine_sequence_additions_preserve_ordering

---
+++ python/determine_sequence_additions_preserve_ordering/solution.py
@@ -0,0 +1,22 @@
+"""
+>>> find_additions([1,2,3], [2,3,4])
+[4]
+>>> find_additions([1,2,3], [3,4,5])
+[4, 5]
+>>> find_additions([1,2,3], [])
+[]
+>>> find_additions([], [1,2,3])
+[1, 2, 3]
+>>> find_additions([], [3,2,1])
+[3, 2, 1]
+"""
+
+def find_additions(prev, cur):
+  prev_set = set(prev)
+  return filter(lambda item: item not in prev_set, cur)
+
+if __name__ == '__main__':
+	import doctest
+	if doctest.testmod().failed > 0:
+		import sys
+		sys.exit(1)
---
+++ python/determine_sequence_additions_preserve_ordering/start.py
@@ -0,0 +1,22 @@
+"""
+>>> find_additions([1,2,3], [2,3,4])
+[4]
+>>> find_additions([1,2,3], [3,4,5])
+[4, 5]
+>>> find_additions([1,2,3], [])
+[]
+>>> find_additions([], [1,2,3])
+[1, 2, 3]
+>>> find_additions([], [3,2,1])
+[3, 2, 1]
+"""
+
+# TODO: Write find_additions() which returns a list containing the items that are in the second sequence 
+# parameter and not in the first sequence parameter which preserves the order of items in the second
+# sequence
+
+if __name__ == '__main__':
+	import doctest
+	if doctest.testmod().failed > 0:
+		import sys
+		sys.exit(1)
