367e509dd3fb2d040aa677bd9f3cfea3e5b63a47
Dan Nuffer <danielnuffer@gmail.com>
2013-01-27 07:54:03 -0700

    Add python/determine_sequence_additions

---
+++ python/determine_sequence_additions/start.py
@@ -0,0 +1,21 @@
+"""
+>>> find_additions([1,2,3], [2,3,4])
+set([4])
+>>> find_additions([1,2,3], [3,4,5])
+set([4, 5])
+>>> find_additions([1,2,3], [])
+set([])
+>>> find_additions([], [1,2,3])
+set([1, 2, 3])
+>>> find_additions([], [3,2,1])
+set([1, 2, 3])
+"""
+
+# TODO: Write find_additions() which returns a set containing the items that are in the second sequence 
+# parameter and not in the first sequence parameter
+
+if __name__ == '__main__':
+	import doctest
+	if doctest.testmod().failed > 0:
+		import sys
+		sys.exit(1)
