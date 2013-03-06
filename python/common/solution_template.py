a3d4a4dbf7126a8a17d6cc31067177fbdd02bdcd
Dan Nuffer <danielnuffer@gmail.com>
2013-03-06 08:42:38 -0700

    Add a python solution template

---
+++ python/common/solution_template.py
@@ -0,0 +1,16 @@
+"""
+>>> print "A test"
+A test
+"""
+
+# TODO
+
+if __name__ == '__main__':
+  import doctest
+  result = doctest.testmod()
+  if result.failed > 0:
+    print "Failed:", result
+    import sys
+    sys.exit(1)
+  else:
+    print "Success:", result
