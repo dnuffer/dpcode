63748edb7763c3915da204a05fc9d005453176b0
Dan Nuffer <danielnuffer@gmail.com>
2013-04-30 07:41:51 -0600

    Add python/calculate_set_difference_itertools

---
+++ python/calculate_set_difference_itertools/solution.py
@@ -0,0 +1,25 @@
+"""
+>>> list(set_difference([1, 2, 3], [1, 2]))
+[]
+>>> list(set_difference([], [1, 2, 3]))
+[1, 2, 3]
+>>> list(set_difference([1, 2, 3], [4, 5, 6]))
+[4, 5, 6]
+"""
+import itertools
+
+# TODO: implement set_difference(first, second) which returns the elements in second which are not in first using itertools
+
+def set_difference(first, second):
+  first_set = set(first)
+  return itertools.ifilter(lambda x: x not in first_set, second)
+
+if __name__ == '__main__':
+  import doctest
+  result = doctest.testmod()
+  if result.failed > 0:
+    print "Failed:", result
+    import sys
+    sys.exit(1)
+  else:
+    print "Success:", result
