f9d023e7a0c00a9343b3c3f97b832ad3040d6a58
Dan Nuffer <danielnuffer@gmail.com>
2013-07-28 13:59:58 -0600

    Implement hello_name_cmd_line/obj-c

--- hello_name_cmd_line/obj-c/solution.m
+++ hello_name_cmd_line/obj-c/solution.m
@@ -1,14 +1,16 @@
 #import <Foundation/Foundation.h>
 
-int main (int argc, const char * argv[])
+int main(int argc, const char * argv[])
 {
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-	NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
-	NSData *inputData = [NSData dataWithData:[input readDataToEndOfFile]];
-	NSString *name = [[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding];
+	if (argc != 3 || strcmp(argv[1], "--name") != 0)
+	{
+		fprintf(stderr, "Invalid command line\n");
+		return 1;
+	}
 
-	printf("Hello, %s\n", [name UTF8String]);
+	printf("Hello, %s\n", argv[2]);
 	[pool drain];
 	return 0;
 }
