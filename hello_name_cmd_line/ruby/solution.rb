35146bb1ad88da5fc521d97165909ef575718933
Dan Nuffer <danielnuffer@gmail.com>
2013-07-28 14:00:29 -0600

    Implement hello_name_cmd_line/ruby

--- hello_name_cmd_line/ruby/solution.rb
+++ hello_name_cmd_line/ruby/solution.rb
@@ -1,2 +1,9 @@
-name = gets
-puts "Hello, #{name}"
+require 'optparse'
+
+OptionParser.new do |o|
+  o.on('--name NAME') { |name| $name = name }
+  o.on('--help') { puts o; exit }
+  o.parse!
+end
+
+puts "Hello, #{$name}"
