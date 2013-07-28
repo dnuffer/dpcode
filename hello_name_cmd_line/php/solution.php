d2cf8afb489271a0a42eb6b32ebfb877994f597e
Dan Nuffer <danielnuffer@gmail.com>
2013-07-28 14:00:12 -0600

    Implement hello_name_cmd_line/php

--- hello_name_cmd_line/php/solution.php
+++ hello_name_cmd_line/php/solution.php
@@ -1,4 +1,10 @@
 <?php
-$name = fgets(STDIN);
-echo "Hello, $name";
+$options = getopt("", array("name:"));
+if (!is_array($options) || !key_exists("name", $options))
+{
+  fprintf(STDERR, "--name was not specified\n");
+  exit(1);
+}
+$name = $options["name"];
+echo "Hello, $name\n";
 ?>
