d9f746c8dee88caa0ba1316b566c6590ff18d3a5
Dan Nuffer <danielnuffer@gmail.com>
2013-08-03 14:47:57 -0600

    Implement hello_name_cmd_line/c#

--- hello_name_cmd_line/c#/solution.cs
+++ hello_name_cmd_line/c#/solution.cs
@@ -1,10 +1,41 @@
 using System;
+using Mono.Options;
 
 public class Hello
 {
-	public static void Main()
+	public static int Main(string[] args)
 	{
-		var name = Console.ReadLine();
+		bool show_help = false;
+		string name = "";
+
+		var p = new OptionSet() {
+			{ "n|name=", "the {NAME} of someone to greet.",
+				v => name = v },
+			{ "h|help",  "show this message and exit", 
+				v => show_help = true },
+		};
+
+		try {
+			p.Parse(args);
+
+			if (!show_help && name == "") {
+				throw new OptionException("missing name", "name");
+			}
+		}
+		catch (OptionException e) {
+			Console.Error.Write("scratch: ");
+			Console.Error.WriteLine(e.Message);
+			Console.Error.WriteLine("Try `scratch --help' for more information.");
+			return 1;
+		}
+
+		if (show_help) {
+			Console.WriteLine("scratch [options]");
+			p.WriteOptionDescriptions(Console.Out);
+			return 0;
+		}
+
 		Console.WriteLine("Hello, " + name);
+		return 0;
 	}
 }
