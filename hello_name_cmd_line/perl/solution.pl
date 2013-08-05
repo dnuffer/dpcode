6dbe24b8e654cb5d1056bd8b443f0c216d2a52ce
Dan Nuffer <danielnuffer@gmail.com>
2013-08-05 07:31:06 -0600

    Implement hello_name_cmd_line/perl

--- hello_name_cmd_line/perl/solution.pl
+++ hello_name_cmd_line/perl/solution.pl
@@ -1,2 +1,26 @@
-$name = <STDIN>;
+use Getopt::Long;
+
+my $name;
+my $show_help = 0;
+
+GetOptions(
+	"n|name=s" => \$name,
+	"h|help" => \$show_help)
+or die("scratch: Invalid command line arguments\nTry `scratch --help' for more information.");
+
+if (!$show_help && $name eq "")
+{
+	print STDERR "scratch: missing name\n";
+	print STDERR "Try `scratch --help' for more information.\n";
+	exit 1;
+}
+
+if ($show_help) 
+{
+	print "Usage: scratch [options]\n";
+	print "  -n, --name NAME          Use NAME to greet someone\n";
+	print "  -h, --help               Print help message\n";
+	exit 0;
+}
+
 print "Hello, $name\n";
