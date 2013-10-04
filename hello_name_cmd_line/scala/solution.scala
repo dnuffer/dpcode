1532b5b804143f1beb760e75d4bc40632ff68a31
Dan Nuffer <danielnuffer@gmail.com>
2013-10-04 08:20:15 -0600

    Implement hello_name_cmd_line/scala

--- hello_name_cmd_line/scala/solution.scala
+++ hello_name_cmd_line/scala/solution.scala
@@ -1,2 +1,33 @@
-var name = readLine
-println("Hello, " + name)
+import org.apache.commons.cli._;
+
+object solution {
+  def main(args: Array[String]) {
+    var options = new Options();
+    options.addOption("n", "name", true, "set name");
+    options.addOption("h", "help", false, "print help");
+    var parser = new BasicParser();
+    try {
+      var cmd = parser.parse(options, args);
+      if (cmd.hasOption("help")) {
+        var formatter = new HelpFormatter();
+        formatter.printHelp("solution [options]", options);
+        System.exit(0);
+      }
+
+      var name = cmd.getOptionValue("name");
+      if (name != null) {
+        System.out.println("Hello, " + name);
+      } else {
+        System.err.println("solution: name not specified");
+        System.err.println("Try `solution --help' for more information.");
+        System.exit(1);
+      }
+    } catch {
+      case ex: ParseException => {
+        System.out.println("solution: Failed to parse command line\n" + ex.getMessage());
+        System.err.println("Try `solution --help' for more information.");
+        System.exit(1);
+      }
+    }
+  }
+}
