190693c05cb9afeff9dcf16ef925a499a088ca20
Dan Nuffer <danielnuffer@gmail.com>
2013-10-05 13:53:42 -0600

    Implement hello_name_cmd_line/scala

--- hello_name_cmd_line/scala/solution.scala
+++ hello_name_cmd_line/scala/solution.scala
@@ -1,33 +1,31 @@
 import org.apache.commons.cli._;
 
-object solution {
-  def main(args: Array[String]) {
-    var options = new Options();
-    options.addOption("n", "name", true, "set name");
-    options.addOption("h", "help", false, "print help");
-    var parser = new BasicParser();
-    try {
-      var cmd = parser.parse(options, args);
-      if (cmd.hasOption("help")) {
-        var formatter = new HelpFormatter();
-        formatter.printHelp("solution [options]", options);
-        System.exit(0);
-      }
+object solution extends App {
+  var options = new Options();
+  options.addOption("n", "name", true, "set name");
+  options.addOption("h", "help", false, "print help");
+  var parser = new BasicParser();
+  try {
+    var cmd = parser.parse(options, args);
+    if (cmd.hasOption("help")) {
+      var formatter = new HelpFormatter();
+      formatter.printHelp("solution [options]", options);
+      System.exit(0);
+    }
 
-      var name = cmd.getOptionValue("name");
-      if (name != null) {
-        System.out.println("Hello, " + name);
-      } else {
-        System.err.println("solution: name not specified");
-        System.err.println("Try `solution --help' for more information.");
-        System.exit(1);
-      }
-    } catch {
-      case ex: ParseException => {
-        System.out.println("solution: Failed to parse command line\n" + ex.getMessage());
-        System.err.println("Try `solution --help' for more information.");
-        System.exit(1);
-      }
+    var name = cmd.getOptionValue("name");
+    if (name != null) {
+      System.out.println("Hello, " + name);
+    } else {
+      System.err.println("solution: name not specified");
+      System.err.println("Try `solution --help' for more information.");
+      System.exit(1);
+    }
+  } catch {
+    case ex: ParseException => {
+      System.out.println("solution: Failed to parse command line\n" + ex.getMessage());
+      System.err.println("Try `solution --help' for more information.");
+      System.exit(1);
     }
   }
 }
