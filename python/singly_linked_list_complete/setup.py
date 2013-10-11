75677652dc15b1a50d97702d04a67356689b6268
Dan Nuffer <danielnuffer@gmail.com>
2013-10-11 17:18:22 -0600

    Make python/singly_linked_list_complete an installable package so that exercises can depend on it.

---
+++ python/singly_linked_list_complete/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name = "singly_linked_list",
+    version = "1.0",
+    description = "Singly Linked List",
+    author="Dan Nuffer",
+    py_modules=["singly_linked_list"]
+)
