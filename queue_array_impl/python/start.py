5c8cba52e0a4a06e0be5e72014ac844b092142e5
Dan Nuffer <danielnuffer@gmail.com>
2013-10-17 08:17:12 -0600

    Do queue_array_impl/python

---
+++ queue_array_impl/python/start.py
@@ -0,0 +1,55 @@
+"""
+>>> q = Queue()
+>>> q.empty()
+True
+>>> q.push_back(1)
+>>> q.empty()
+False
+>>> q.pop_front()
+1
+>>> q.empty()
+True
+>>> q.push_back(2)
+>>> q.push_back(3)
+>>> q.push_back(4)
+>>> q.empty()
+False
+>>> q.pop_front()
+2
+>>> q.pop_front()
+3
+>>> q.pop_front()
+4
+>>> q.empty()
+True
+>>> xin = 0
+>>> expect_out = 0
+>>> for x in xrange(1, 100):
+...   for y in xrange(x):
+...     q.push_back(xin)
+...     xin += 1
+...   xout = q.pop_front()
+...   if xout != expect_out:
+...     print "Failed: got %d, expected %d" % (xout, expect_out)
+...     break
+...   expect_out += 1
+>>> while not q.empty():
+...   xout = q.pop_front()
+...   if xout != expect_out:
+...     print "Failed: got %d, expected %d" % (xout, expect_out)
+...     break
+...   expect_out += 1
+"""
+
+# TODO: copy this to solution.py when it is done next
+
+if __name__ == "__main__":
+  import doctest
+  result = doctest.testmod()
+  if result.failed > 0:
+    print "Failed:", result
+    import sys
+    sys.exit(1)
+  else:
+    print "Success:", result
+
