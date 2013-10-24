cfe4f3278d500f6571e499bcdf4319a3e9180bc1
Dan Nuffer <danielnuffer@gmail.com>
2013-10-24 08:55:22 -0600

    Completed javascript singly linked list.

--- js/singly_linked_list_complete/lib/singly_linked_list.js
+++ js/singly_linked_list_complete/lib/singly_linked_list.js
@@ -7,11 +7,16 @@ function SinglyLinkedListNode(data) {
 exports.SinglyLinkedList = function(items) {
   this.head = null;
   this.tail = null;
-}
+  if (items) {
+    for (var i = 0; i < items.length; i++) {
+      this.append(items[i]);
+    }
+  }
+};
 
 exports.SinglyLinkedList.prototype.append = function(data) {
   return this.append_node(new SinglyLinkedListNode(data));
-}
+};
 
 exports.SinglyLinkedList.prototype.append_node = function(node) {
   if (this.tail) {
@@ -21,5 +26,140 @@ exports.SinglyLinkedList.prototype.append_node = function(node) {
     this.head = this.tail = node;
   }
   return this;
-}
+};
 
+exports.SinglyLinkedList.prototype.toString = function() {
+  var result = "[";
+  var node = this.head;
+  if (node) {
+    result += JSON.stringify(node.data);
+    node = node.next_node;
+    while (node) {
+      result += " -> ";
+      result += JSON.stringify(node.data);
+      node = node.next_node;
+    }
+  }
+  result += "]";
+  return result;
+};
+
+exports.SinglyLinkedList.prototype.insert = function(position, data) {
+  if (position == 0 && !this.tail) {
+    this.append(data);
+  } else {
+    var node = this.head;
+    var prev = null;
+    while (node && position > 0) {
+      prev = node;
+      node = node.next_node;
+      position -= 1;
+    }
+    if (prev) {
+      prev.next_node = new SinglyLinkedListNode(data);
+      prev.next_node.next_node = node;
+      if (prev == this.tail) {
+        this.tail = prev.next_node;
+      }
+    } else {
+      var old_head = this.head;
+      this.head = new SinglyLinkedListNode(data);
+      this.head.next_node = old_head;
+    }
+  }
+  return this;
+};
+
+exports.SinglyLinkedList.prototype.delete = function(data) {
+  if (!this.head) {
+    return this;
+  }
+
+  if (this.head.data === data) {
+    if (this.head === this.tail) {
+      this.head = this.tail = null;
+    } else {
+      this.head = this.head.next_node;
+    }
+  } else {
+    var node = this.head;
+    while (node.next_node) {
+      if (node.next_node.data === data) {
+        if (!node.next_node.next_node) {
+          this.tail = node;
+        }
+        node.next_node = node.next_node.next_node;
+      } else {
+        node = node.next_node;
+      }
+    }
+  }
+  return this;
+};
+
+Object.defineProperty(exports.SinglyLinkedList.prototype, "length", {
+  get: function() {
+    var len = 0;
+    var node = this.head;
+    while (node) {
+      len++;
+      node = node.next_node;
+    }
+    return len;
+  }
+});
+
+exports.SinglyLinkedList.prototype.getItem = function(index) {
+  if (index >= 0) {
+    var node = this.head;
+    while (index > 0 && node)
+    {
+      node = node.next_node;
+      index--;
+    }
+    if (node) {
+      return node.data;
+    }
+    return;
+  } else {
+    var len = this.length;
+    if (index * -1 > len) {
+      return;
+    }
+    return this.getItem(len + index);
+  }
+};
+
+exports.SinglyLinkedList.prototype.forEach = function forEach(callback, thisArg) {
+  'use strict';
+  var T, k;
+
+  if (this == null) {
+    throw new TypeError("this is null or not defined");
+  }
+
+  var kValue,
+      // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+      O = Object(this);
+
+  // 4. If IsCallable(callback) is false, throw a TypeError exception.
+  // See: http://es5.github.com/#x9.11
+  if ({}.toString.call(callback) !== "[object Function]") {
+    throw new TypeError(callback + " is not a function");
+  }
+
+  // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+  if (arguments.length >= 2) {
+    T = thisArg;
+  }
+
+  var node = O.head;
+  k = 0;
+
+  while (node) {
+    kValue = node.data;
+    callback.call(T, kValue, k, O);
+    node = node.next_node;
+    k++;
+  }
+};
--- js/singly_linked_list_complete/test/singly_linked_list.test.js
+++ js/singly_linked_list_complete/test/singly_linked_list.test.js
@@ -1,15 +1,184 @@
 require("should");
 var sll = require("../");
 var assert = require("assert");
+var SinglyLinkedList = sll.SinglyLinkedList;
 
 describe("SinglyLinkedList", function() {
   describe("#append()", function() {
     it("should append to an empty list", function() {
-      var l = new sll.SinglyLinkedList();
+      var l = new SinglyLinkedList();
       l.append(0);
       l.head.data.should.equal(0);
       assert(!l.head.next_node);
       l.head.should.equal(l.tail);
+      l.toString().should.equal("[0]");
+      l.append(null);
+      l.toString().should.equal("[0 -> null]");
+      l.append("a");
+      l.toString().should.equal("[0 -> null -> \"a\"]");
+    });
+  });
+
+  describe("#toString()", function() {
+    it("should make a string", function() {
+      var l = new SinglyLinkedList([0, null, "a"]);
+      l.toString().should.equal("[0 -> null -> \"a\"]");
+      var l = new SinglyLinkedList([]);
+      l.toString().should.equal("[]");
+      var l = new SinglyLinkedList(["I am a funny bunch of coconuts"]);
+      l.toString().should.equal("[\"I am a funny bunch of coconuts\"]");
+      var l = new SinglyLinkedList([[1, "list"]]);
+      l.toString().should.equal("[[1,\"list\"]]");
+    });
+  });
+
+  describe("#SinglyLinkedList", function() {
+    it("takes an array as a constructor parameter", function() {
+      var l = new SinglyLinkedList([0, null, "a"]);
+      l.toString().should.equal("[0 -> null -> \"a\"]");
+    });
+  });
+
+  describe("#append_node", function() {
+    it("appends a node", function() {
+      var l1 = new SinglyLinkedList([1,2]);
+      var l2 = new SinglyLinkedList([3,4]);
+      l1.append_node(l2.head);
+      l1.toString().should.equal("[1 -> 2 -> 3 -> 4]");
+    });
+  });
+
+  describe("#insert", function() {
+    it("inserts at 0 on an empty list", function() {
+      var l = new SinglyLinkedList();
+      l.insert(0, 'begin');
+      l.toString().should.equal("[\"begin\"]");
+    });
+    it("inserts at 0 a list with 1 item", function() {
+      var l = new SinglyLinkedList([1]);
+      l.insert(0, 'begin');
+      l.toString().should.equal("[\"begin\" -> 1]");
+    });
+    it("inserts at 0 a list with 2 items", function() {
+      var l = new SinglyLinkedList([1, 2]);
+      l.insert(0, 'begin');
+      l.toString().should.equal("[\"begin\" -> 1 -> 2]");
+    });
+    it("inserts at end of a list with 1 item", function() {
+      var l = new SinglyLinkedList(['begin']);
+      l.insert(1, "second");
+      l.toString().should.equal("[\"begin\" -> \"second\"]");
+    });
+    it("inserts at end of a list with 2 items", function() {
+      var l = new SinglyLinkedList([1, 2]);
+      l.insert(2, "end");
+      l.toString().should.equal("[1 -> 2 -> \"end\"]");
+    });
+    it("inserts in the middle of a list with 2 items", function() {
+      var l = new SinglyLinkedList([1, 2]);
+      l.insert(1, "middle");
+      l.toString().should.equal("[1 -> \"middle\" -> 2]");
+    });
+  });
+
+  describe("#delete", function() {
+    it("returns itself when called on an empty list", function() {
+      l = new SinglyLinkedList();
+      var res = l.delete(999);
+      l.toString().should.equal("[]");
+      res.should.equal(l);
+    });
+    it("deletes the first item from a list of 2 items", function() {
+      l = new SinglyLinkedList([10, 20]);
+      l.delete(10);
+      l.toString().should.equal("[20]");
+    });
+    it("makes no changes if data is not in the list", function() {
+      l = new SinglyLinkedList([10, 20]);
+      l.delete(99);
+      l.toString().should.equal("[10 -> 20]");
+    });
+    it("deletes the first item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.delete(1);
+      l.toString().should.equal("[2 -> 3]");
+    });
+    it("deletes the middle item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.delete(2);
+      l.toString().should.equal("[1 -> 3]");
+    });
+    it("deletes the last item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.delete(3);
+      l.toString().should.equal("[1 -> 2]");
+    });
+  });
+
+  describe("#length", function() {
+    it("returns 0 on an empty list", function() {
+      new SinglyLinkedList().length.should.equal(0);
+    });
+    it("returns 1 on a size 1 list", function() {
+      new SinglyLinkedList([1]).length.should.equal(1);
+    });
+    it("returns 3 on a size 3 list", function() {
+      new SinglyLinkedList([1, 2, 3]).length.should.equal(3);
+    });
+  });
+
+  describe("#getItem", function() {
+    it("returns the first item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.getItem(0).should.equal(1);
+    });
+    it("returns the middle item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.getItem(1).should.equal(2);
+    });
+    it("returns the last item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.getItem(2).should.equal(3);
+    });
+    it("returns undefined", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      assert(l.getItem(3) === undefined);
+    });
+    it("returns the first item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.getItem(-3).should.equal(1);
+    });
+    it("returns the middle item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.getItem(-2).should.equal(2);
+    });
+    it("returns the last item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      l.getItem(-1).should.equal(3);
+    });
+    it("returns undefined", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      assert(l.getItem(-10) === undefined);
+    });
+  });
+
+  describe("#forEach", function() {
+    it("calls the callback for every item", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      var values = [];
+      l.forEach(function(value, index, list) {
+        values[index] = value;
+        list.should.equal(l);
+      });
+      values.should.eql([1, 2, 3]);
+    });
+
+    it("calls the callback with thisArg", function() {
+      l = new SinglyLinkedList([1, 2, 3]);
+      var thisArg = new Object();
+      l.forEach(function(value, index, list) {
+        this.should.equal(thisArg);
+      }, thisArg);
     });
   });
 });
