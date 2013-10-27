fa427641bfd6605dab1921859d5f0d739ed7778e
Dan Nuffer <danielnuffer@gmail.com>
2013-10-27 10:31:47 -0600

    Run make clean before make check-solution

--- check_all.sh
+++ check_all.sh
@@ -5,9 +5,17 @@ list_make_targets()
 	make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}'
 }
 
+have_make_target()
+{
+	list_make_targets | grep -q "$1" 2>/dev/null
+}
+
 while read -r dir_to_check; do
 	pushd $dir_to_check
-	if list_make_targets | grep -q check-solution; then
+	if have_make_target clean; then
+		make clean
+	fi
+	if have_make_target check-solution; then
 		if ! make check-solution; then
 			echo "Dir $dir_to_check failed!"
 			break;
