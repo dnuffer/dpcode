b070e57ef202a61069448e7b2f1db5e99f2b4069
Dan Nuffer <danielnuffer@gmail.com>
2013-10-27 13:24:36 -0600

    Update hello_name_cmd_line/php to pass check-solution

--- hello_name_cmd_line/php/solution.php
+++ hello_name_cmd_line/php/solution.php
@@ -2,15 +2,15 @@
 $options = getopt("n:h", array("name:", "help"));
 function usage($out)
 {
-	fprintf($out, "usage: solution\n");
+	fprintf($out, "usage: solution [options]\n");
 	fprintf($out, " -h,--help         print help\n");
 	fprintf($out, " -n,--name <arg>   set name\n");
 }
 
 if (!is_array($options))
 {
-  fprintf(STDERR, "failed to parse options\n");
-  usage(STDERR);
+  fprintf(STDERR, "solution: failed to parse options\n");
+  fprintf(STDERR, "Try `solution --help' for more information.\n");
   exit(1);
 }
 
@@ -22,8 +22,8 @@ if (key_exists("help", $options))
 
 if (!key_exists("name", $options) && !key_exists("n", $options))
 {
-  fprintf(STDERR, "--name was not specified\n");
-  usage(STDERR);
+  fprintf(STDERR, "solution: --name was not specified\n");
+  fprintf(STDERR, "Try `solution --help' for more information.\n");
   exit(1);
 }
 
