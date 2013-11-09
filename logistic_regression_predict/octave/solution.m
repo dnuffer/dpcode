e67383f18315c03b872c39154ea3f2e710450e58
Dan Nuffer <danielnuffer@gmail.com>
2013-11-09 14:48:22 -0700

    Add logistic_regression_{cost,gradient,predict}

---
+++ logistic_regression_predict/octave/solution.m
@@ -0,0 +1,28 @@
+function p = predict(theta, X)
+%PREDICT Predict whether the label is 0 or 1 using learned logistic 
+%regression parameters theta
+%   p = PREDICT(theta, X) computes the predictions for X using a 
+%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+
+m = size(X, 1); % Number of training examples
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters. 
+%               You should set p to a vector of 0's and 1's
+%
+
+p = (sigmoid(theta'*X') >= 0.5)';
+
+
+
+% =========================================================================
+
+
+end
+
+%!test
+%! t = [  -25.16154; 0.20623; 0.20147];
+%! assert(predict(t, [1.0 45 85]), 1.0, 1e-2);
+%! assert(predict(t, [1.0 45 85; 1.0 30 30]), [1.0; 0.0], 1e-2);
+
