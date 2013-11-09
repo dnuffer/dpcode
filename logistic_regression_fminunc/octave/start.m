ce2d86d1f3ac7c3a20c840a99fc601032a5feb24
Dan Nuffer <danielnuffer@gmail.com>
2013-11-09 15:20:34 -0700

    Add logistic_regression_fminunc/octave

---
+++ logistic_regression_fminunc/octave/start.m
@@ -0,0 +1,19 @@
+function [J, grad] = costFunction(theta, X, y)
+m = length(y); % number of training examples
+J = -(1 / m) * ( log(    sigmoid(X * theta))' * y +
+					 log(1 - sigmoid(X * theta))' * (1 - y)
+				  );
+
+grad = (1 / m) * X' * ( sigmoid(X * theta) - y );
+end
+
+test scratch
+%!test
+%! data = load('ex2data1.txt');
+%! X = data(:, [1, 2]); 
+%! y = data(:, 3);
+%! [m, n] = size(X);
+%! X = [ones(m, 1) X];
+%! TODO: call fminunc 
+%! assert(theta, [-25.161537; 0.206233; 0.201474], 1e-6);
+%! assert(cost, 0.203498, 1e-6);
