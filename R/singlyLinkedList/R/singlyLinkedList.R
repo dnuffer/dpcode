a249875222c9aab8c1261e2b81f95de6673a08b9
Dan Nuffer <danielnuffer@gmail.com>
2013-11-10 14:11:48 -0700

    Put together initial version of a R/singlyLinkedList package

---
+++ R/singlyLinkedList/R/singlyLinkedList.R
@@ -0,0 +1,5 @@
+setRefClass("singlyLinkedList",
+				fields = list(
+								  head="list"
+								  )
+				)
---
+++ R/singlyLinkedList/tests/run-all.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(singlyLinkedList)
+
+test_package("singlyLinkedList")
