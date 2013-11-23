c241df2e5e911f712200b17bf3fb31b68929dfd8
Dan Nuffer <danielnuffer@gmail.com>
2013-11-23 13:23:10 -0700

    Tweak hello_name_cmd_line/c#/solution.cs

--- hello_name_cmd_line/c#/solution.cs
+++ hello_name_cmd_line/c#/solution.cs
@@ -1,32 +1,40 @@
 using System;
 using Mono.Options;
 
-public class Hello {
-	public static int Main(string[] args) {
+public class Hello 
+{
+	public static int Main(string[] args) 
+	{
 		bool show_help = false;
 		string name = "";
 
-		var p = new OptionSet() {
+		var p = new OptionSet() 
+		{
 			{ "n|name=", "the {NAME} of someone to greet.",
 				v => name = v },
 			{ "h|help",  "show this message and exit", 
 				v => show_help = true },
 		};
 
-		try {
+		try 
+		{
 			p.Parse(args);
 
-			if (!show_help && name == "") {
+			if (!show_help && name == "") 
+			{
 				throw new OptionException("missing name", "name");
 			}
-		} catch (OptionException e) {
+		} 
+		catch (OptionException e) 
+		{
 			Console.Error.Write("scratch: ");
 			Console.Error.WriteLine(e.Message);
 			Console.Error.WriteLine("Try `scratch --help' for more information.");
 			return 1;
 		}
 
-		if (show_help) {
+		if (show_help) 
+		{
 			Console.WriteLine("Usage: scratch [options]");
 			p.WriteOptionDescriptions(Console.Out);
 			return 0;
