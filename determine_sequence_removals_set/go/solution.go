435c1e0e6f6de388681994101e3d50c4ef4d91e9
Dan Nuffer <danielnuffer@gmail.com>
2013-11-24 16:20:06 -0700

    Use import dot to avoid needing to specify gocheck repeatedly.

--- determine_sequence_removals_set/go/solution.go
+++ determine_sequence_removals_set/go/solution.go
@@ -1,6 +1,8 @@
 package main
 
-import "launchpad.net/gocheck"
+import (
+	. "launchpad.net/gocheck"
+)
 
 func determine_sequence_removals(first, second map[int]bool) (removals map[int]bool) {
 	removals = make(map[int]bool)
@@ -14,6 +16,6 @@ func determine_sequence_removals(first, second map[int]bool) (removals map[int]b
 }
 
 func main() {
-	c := gocheck.C{}
-	c.Assert(determine_sequence_removals(map[int]bool {1:true, 2:true, 3:true}, map[int]bool {1:true, 2:true}), gocheck.DeepEquals, map[int]bool {3:true})
+	c := C{}
+	c.Assert(determine_sequence_removals(map[int]bool {1:true, 2:true, 3:true}, map[int]bool {1:true, 2:true}), DeepEquals, map[int]bool {3:true})
 }
