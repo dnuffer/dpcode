897e5cbd1efec6ef05a07ff6c3f54cbb6376ba5a
Dan Nuffer <danielnuffer@gmail.com>
2013-12-27 11:34:40 -0700

    Rename Y->y in linear_regression_compute_cost/R/solution.r

--- linear_regression_compute_cost/R/solution.r
+++ linear_regression_compute_cost/R/solution.r
@@ -1,14 +1,14 @@
 library(testthat)
 
-compute_cost = function(X, Y, theta) {
-  tmp1 = X %*% theta - Y
-  J = 0.5 / nrow(Y) * t(tmp1) %*% tmp1
+compute_cost = function(X, y, theta) {
+  tmp1 = X %*% theta - y
+  J = 0.5 / nrow(y) * t(tmp1) %*% tmp1
   return(J[1,1])
 }
 
 test_that("compute_cost works", {
           X = matrix(c(10,20,300,400), nrow=2, ncol=2, byrow=TRUE)
-          Y = matrix(c(5,50), nrow=2, ncol=1)
+          y = matrix(c(5,50), nrow=2, ncol=1)
           theta = matrix(c(5,2), nrow=2, ncol=1)
-          expect_that(compute_cost(X, Y, theta), equals(1267431.25))
+          expect_that(compute_cost(X, y, theta), equals(1267431.25))
 })
