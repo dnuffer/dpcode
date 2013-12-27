490b8b38a319759c6d0d95d7814ca504ebffd4dd
Dan Nuffer <danielnuffer@gmail.com>
2013-12-27 12:33:19 -0700

    Add find_missing_solutions.rb

---
+++ find_missing_solutions.rb
@@ -0,0 +1,21 @@
+require 'find'
+scratch_files = Find.find(".").
+  find_all { |fn| fn =~ /^\.\/[^\/]+\/[^\/]+\/scratch\.[^.]+$/ }.
+  find_all { |fn| File.size?(fn) }.
+  to_a
+
+scratch_dirs = scratch_files.map { |fn| File.dirname(fn) }.uniq
+
+solution_files = Find.find(".").
+  find_all { |fn| fn =~ /^\.\/[^\/]+\/[^\/]+\/solution\.[^.]+$/ }.
+  find_all { |fn| File.size?(fn) }.
+  to_a
+
+solution_dirs = solution_files.map { |fn| File.dirname(fn) }.uniq
+
+scratch_no_solution = scratch_dirs - solution_dirs
+if scratch_no_solution.length > 0
+  puts (scratch_no_solution).join("\n")
+else
+  puts "No missing solutions found"
+end
