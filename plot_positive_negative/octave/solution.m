b258b021c9d1b789103c7875bcdef2af7a21aa62
Dan Nuffer <danielnuffer@gmail.com>
2013-12-31 09:35:30 -0700

    Implement plot_positive_negative/octave

--- plot_positive_negative/octave/solution.m
+++ plot_positive_negative/octave/solution.m
@@ -1,41 +1,3 @@
-function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
-%   PLOTDATA(x,y) plots the data points with + for the positive examples
-%   and o for the negative examples. X is assumed to be a Mx2 matrix.
-
-% Create New Figure
-figure; hold on;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
-
-pos = find(y==1);
-neg = find(y==0);
-
-plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
-plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
-
-
-
-
-
-
-
-
-% =========================================================================
-
-
-
-hold off;
-
-end
-
-%% Initialization
-clear ; close all; clc
-
 %% Load Data
 %  The first two columns contains the exam scores and the third column
 %  contains the label.
@@ -50,10 +12,22 @@ X = data(:, [1, 2]); y = data(:, 3);
 fprintf(['Plotting data with + indicating (y = 1) examples and o ' ...
          'indicating (y = 0) examples.\n']);
 
-plotData(X, y);
+% Create New Figure
+figure; hold on;
 
-% Put some labels 
-hold on;
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+pos = find(y==1);
+neg = find(y==0);
+plot(X(pos,1), X(pos,2), "k+", 'LineWidth', 2, "MarkerSize", 7);
+plot(X(neg,1), X(neg,2), "ko", "MarkerFaceColor", "y", "MarkerSize", 7);
+
+
+
+
 % Labels and Legend
 xlabel('Exam 1 score')
 ylabel('Exam 2 score')
