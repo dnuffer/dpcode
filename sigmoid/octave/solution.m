6a9381d41e4f8c321e055bdca4aa23c8fe618b35
Dan Nuffer <danielnuffer@gmail.com>
2013-12-31 09:33:29 -0700

    Remove sigmoid noisy comments

--- sigmoid/octave/solution.m
+++ sigmoid/octave/solution.m
@@ -2,15 +2,7 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
-
 g = 1 ./ (1 + exp(-z));
-
-
-
-% =============================================================
 
 end
 
--- sigmoid/octave/start.m
+++ sigmoid/octave/start.m
@@ -2,14 +2,7 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
 
-
-
-
-% =============================================================
 
 end
 
