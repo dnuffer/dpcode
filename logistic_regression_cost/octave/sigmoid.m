4c31718a2c9b1b47cbb9a05e7c060514b4ae0402
Dan Nuffer <danielnuffer@gmail.com>
2014-01-03 16:01:16 -0700

    Implement logistic_regression_cost/octave

---
+++ logistic_regression_cost/octave/sigmoid.m
@@ -0,0 +1,1 @@
+../../common/octave/sigmoid.m
--- logistic_regression_cost/octave/start.m
+++ logistic_regression_cost/octave/start.m
@@ -4,26 +4,11 @@ function [J, grad] = costFunction(theta, X, y)
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
 
-% Initialize some useful values
 m = length(y); % number of training examples
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
+% TODO: calculate the cost and assign it to J
 
 grad = (1 / m) * X' * ( sigmoid(X * theta) - y );
-
-
-
-
-% =============================================================
 
 end
 
