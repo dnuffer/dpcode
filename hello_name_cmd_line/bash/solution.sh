922891e333dc957e626e82e2f058dadb8840b103
Dan Nuffer <danielnuffer@gmail.com>
2014-01-10 09:07:43 -0700

    Fix up hello_name_cmd_line/bash

--- hello_name_cmd_line/bash/solution.sh
+++ hello_name_cmd_line/bash/solution.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-TEMP=`getopt -o n: --long name: -n 'solution.sh' -- "$@"`
+TEMP=`getopt -o -n:h --long name:,help -n 'solution' -- "$@"`
 
-if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+if [ $? != 0 ]; then echo "Try \`solution --help' for more information." >&2; exit 1; fi
 
 # Note the quotes around `$TEMP': they are essential!
 eval set -- "$TEMP"
 
 NAME=
+SHOW_HELP="no"
 while true; do
-  case "$1" in
-    --name ) NAME="$2"; shift 2 ;;
-    -- ) shift; break ;;
-    -*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
-    * ) break ;;
-  esac
+	case "$1" in
+		--name|-n ) NAME="$2"; shift 2;;
+		--help|-h ) SHOW_HELP="yes"; shift;;
+		-- ) shift; break;;
+		-*) echo "solution: error - unrecognized option $1" 1>&2; echo "Try \`solution --help' for more information" 1>&2; exit 1;;
+		* ) break;;
+	esac
 done
 
-if [ "z${NAME}" = "z" ]; then echo "Name not specified" >&2; exit 1; fi
+if [ "${SHOW_HELP}" = "yes" ]; then
+	echo "Usage: solution [options]"
+	echo "  --name, -n {NAME}    Show to a greeting to {NAME}"
+	echo "  --help, -h           Display a help message"
+	exit 0
+fi
+
+if [ "z${NAME}" = "z" ]; then 
+	echo "solution: Name not specified" >&2
+	echo "Try \`solution --help' for more information." >&2
+	exit 1
+fi
 
 echo "Hello, ${NAME}"
