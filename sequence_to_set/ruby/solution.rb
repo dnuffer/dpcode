812dae10997bcd5fb51b555dd3445349ba994820
Dan Nuffer <danielnuffer@gmail.com>
2014-01-15 22:34:52 -0700

    Implement sequence_to_set/ruby

---
+++ sequence_to_set/ruby/solution.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+
+require 'set'
+
+def to_set(enum)
+  Set.new enum
+end
+
+describe "to_set" do
+  it "makes a set out of an array" do
+    s = to_set([1,2,3])
+    s.size.should == 3
+    s.to_a.sort.should == [1,2,3]
+  end
+
+  it "makes a set out of an enumerable" do
+    s = to_set((1..10).each)
+    s.size.should == 10
+    s.to_a.sort.should == [1,2,3,4,5,6,7,8,9,10]
+  end
+
+  it "makes a set out of a range" do
+    s = to_set((1..4))
+    s.size.should == 4
+    s.to_a.sort.should == [1,2,3,4]
+  end
+end
+
+if $0 == __FILE__
+  system "rspec #{$0}"
+end
