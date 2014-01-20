c68137ee613fe0e93bbbf5d75c556b6a97ffcb68
Dan Nuffer <danielnuffer@gmail.com>
2014-01-20 09:32:32 -0700

    Implement sigmoid/python

---
+++ sigmoid/python/solution.py
@@ -0,0 +1,34 @@
+"""
+>>> sigmoid(0)
+0.5
+>>> sigmoid(1)
+0.7310585786300049
+>>> sigmoid(-1)
+0.2689414213699951
+>>> sigmoid(array([1, 10, 100]))
+array([ 0.73105858,  0.9999546 ,  1.        ])
+>>> sigmoid(array([[1], [10], [100]]))
+array([[ 0.73105858],
+       [ 0.9999546 ],
+       [ 1.        ]])
+>>> sigmoid(array([[1, 2], [3, 4]]))
+array([[ 0.73105858,  0.88079708],
+       [ 0.95257413,  0.98201379]])
+
+"""
+
+from numpy import *
+
+def sigmoid(z):
+  return 1. / (1. + exp(-z))
+
+
+if __name__ == '__main__':
+  import doctest
+  result = doctest.testmod()
+  if result.failed > 0:
+    print "Failed:", result
+    import sys
+    sys.exit(1)
+  else:
+    print "Success:", result
