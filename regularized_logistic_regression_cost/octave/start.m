58148fef3e957b1873acda0fd478b863f38c59f9
Dan Nuffer <danielnuffer@gmail.com>
2014-01-23 08:55:11 -0700

    Remove annoying comments

--- regularized_logistic_regression_cost/octave/start.m
+++ regularized_logistic_regression_cost/octave/start.m
@@ -1,21 +1,11 @@
 function [J, grad] = costFunctionReg(theta, X, y, lambda)
-%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
-%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
-%   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
 
 m = length(y); % number of training examples
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+% TODO: Set J to the cost
 
 
 grad = (1 / m) * X' * ( sigmoid(X * theta) - y ) + (lambda / m * thetaReg);
-
-% =============================================================
 
 end
 
--- regularized_logistic_regression_gradient/octave/sigmoid.m
+++ regularized_logistic_regression_gradient/octave/sigmoid.m
@@ -1,16 +1,6 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid functoon
-%   J = SIGMOID(z) computes the sigmoid of z.
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
 
 g = 1 ./ (1 + exp(-z));
-
-
-
-% =============================================================
 
 end
 
