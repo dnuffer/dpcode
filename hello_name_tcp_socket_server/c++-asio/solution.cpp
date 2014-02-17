0c1f0dd507eaf1a61b304e613b35fffdc91c7359
Dan Nuffer <danielnuffer@gmail.com>
2014-02-17 08:29:20 -0700

    simplify hello_name_tcp_socket_server/c++-asio

--- hello_name_tcp_socket_server/c++-asio/solution.cpp
+++ hello_name_tcp_socket_server/c++-asio/solution.cpp
@@ -1,70 +1,46 @@
 #include <cstdlib>
 #include <iostream>
-#include <thread>
 #include <utility>
 #include <boost/asio.hpp>
 
+using namespace std;
 using boost::asio::ip::tcp;
 
 const int max_length = 1024;
 
-void session(std::shared_ptr<tcp::socket> sock)
+void session(tcp::socket sock)
 {
-	try
+	string hello = "Hello, ";
+	boost::asio::write(sock, boost::asio::buffer(hello.c_str(), hello.size()));
+	for (;;)
 	{
 		char data[max_length];
 
 		boost::system::error_code error;
-		size_t length = sock->read_some(boost::asio::buffer(data), error);
+		size_t length = sock.read_some(boost::asio::buffer(data), error);
 		if (error == boost::asio::error::eof)
-			return; // Connection closed cleanly by peer.
+			break; // Connection closed cleanly by peer.
 		else if (error)
 			throw boost::system::system_error(error); // Some other error.
 
-		std::string response = "Hello, " + std::string(data, length);
-		if (response[response.size()-1] != '\n')
-			response += "\n";
-
-		boost::asio::write(*sock, boost::asio::buffer(response, response.size()));
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "Exception in thread: " << e.what() << "\n";
+		boost::asio::write(sock, boost::asio::buffer(data, length));
 	}
 }
 
 void server(boost::asio::io_service& io_service, unsigned short port)
 {
 	tcp::acceptor a(io_service, tcp::endpoint(tcp::v4(), port));
-	std::cout << "Ready" << std::endl;
-	for (;;)
-	{
-		std::shared_ptr<tcp::socket> sock(new tcp::socket(io_service));
-		a.accept(*sock);
-		std::thread t(session, std::move(sock));
-		t.join();
-		return;
-	}
+	cout << "Ready" << endl;
+	tcp::socket sock(io_service);
+	a.accept(sock);
+	session(move(sock));
 }
 
 int main(int argc, char* argv[])
 {
-	try
-	{
-		if (argc != 2)
-		{
-			std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
-			return 1;
-		}
+	boost::asio::io_service io_service;
 
-		boost::asio::io_service io_service;
-
-		server(io_service, std::atoi(argv[1]));
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << "\n";
-	}
+	server(io_service, 9999);
 
 	return 0;
 }
