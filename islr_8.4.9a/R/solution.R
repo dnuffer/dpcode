54064aa37b96cf23843c9dc76e51ce3583d7e001
Dan Nuffer <danielnuffer@gmail.com>
2014-05-22 21:24:13 -0600

    Add islr_8.4.9a

---
+++ islr_8.4.9a/R/solution.R
@@ -0,0 +1,6 @@
+library(caret)
+library(ISLR)
+data(OJ)
+train=createDataPartition(OJ$Purchase, p=0.8, list=FALSE)
+OJ.train=OJ[train,]
+OJ.test=OJ[-train,]
---
+++ islr_8.4.9a/R/start.R
@@ -0,0 +1,4 @@
+library(caret)
+library(ISLR)
+data(OJ)
+# TODO: Create a training set containing a random sample of 80% of the observations, and a test set with the reamining.
