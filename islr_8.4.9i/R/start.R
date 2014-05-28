55833062a76d98018bee982fe620efa218ee48a3
Dan Nuffer <danielnuffer@gmail.com>
2014-05-27 21:59:25 -0600

    Fix islr_8.4.9i/R TODO message.

--- islr_8.4.9i/R/start.R
+++ islr_8.4.9i/R/start.R
@@ -4,4 +4,4 @@ data(OJ)
 train=createDataPartition(OJ$Purchase, p=0.8, list=FALSE)
 OJ.train=OJ[train,]
 OJ.test=OJ[-train,]
-# TODO: Produce a plot with tree size on the x-axis and cross-validated classification error rate on the y-axis.
+# TODO: Produce a pruned tree corresponding to the optimal tree size obtained using cross-validation. If cross-validation does not lead to selection of a pruned tree, then create a pruned tree with five terminal nodes.
