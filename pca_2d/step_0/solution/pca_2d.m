80408220341caec16157233b98b1c55d69cdb137
Dan Nuffer <danielnuffer@gmail.com>
2015-03-02 08:59:42 -0700

    Fix comment.

--- pca_2d/step_0/solution/pca_2d.m
+++ pca_2d/step_0/solution/pca_2d.m
@@ -2,10 +2,8 @@ close all
 
 %%================================================================
 %% Step 0: Load data
-%  We have provided the code to load data from pcaData.txt into x.
 %  x is a 2 * 45 matrix, where the kth column x(:,k) corresponds to
-%  the kth data point.Here we provide the code to load natural image data into x.
-%  You do not need to change the code below.
+%  the kth data point.
 
 x = load('pcaData.txt','-ascii');
 figure(1);
